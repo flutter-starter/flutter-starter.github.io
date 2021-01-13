bf063482e8f79c00c1ce44a5babedf17e168b641
madhav23bansal <madhavb230100@gmail.com>
2021-01-13 20:04:52 +0530

    Added Google Tag Manager

--- js/google-tag-manager.js
+++ js/google-tag-manager.js
@@ -1,13 +1,22 @@
 window.onload = () => {
-  var script = document.createElement("script");
-  var t =
-    "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PJ983WS');";
-  script.appendChild(t);
-  document.head.appendChild(script);
+  (function (w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+    var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l != "dataLayer" ? "&l=" + l : "";
+    j.async = true;
+    j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+    f.parentNode.insertBefore(j, f);
+  })(window, document, "script", "dataLayer", "GTM-PJ983WS");
 
   var noscript = document.createElement("noscript");
-  var t2 =
-    "<noscript><iframe src='https://www.googletagmanager.com/ns.html?id=GTM-PJ983WS' height='0' width='0' style='display:none;visibility:hidden'><iframe><noscript>";
-  noscript.appendChild(t2);
+  var iframe = document.createElement("iframe");
+  iframe.src = 'https://www.googletagmanager.com/ns.html?id=GTM-PJ983WS';
+  iframe.height = '0';
+  iframe.width = '0';
+  iframe.style.display = 'none';
+  iframe.style.visibility = 'hidden';
+  noscript.appendChild(iframe);
   document.body.appendChild(noscript);
 };
